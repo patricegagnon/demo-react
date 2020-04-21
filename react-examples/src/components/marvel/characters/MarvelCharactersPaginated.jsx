ce28243f76ee08548d535e5d76562b42792b20f5
Patrice Gagnon <patrice.gagnon@alithya.com>
2020-04-21 16:33:26 -0400

    Ajout de React exemple seul

---
+++ react-examples/src/components/marvel/characters/MarvelCharactersPaginated.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import MarvelService, {getInstance, MarvelImageFormats, getImageUrl} from '../../../services/MarvelProxyService'
+import {Link} from "react-router-dom"
+import Pagination from '../../common/Pagination'
+
+import {CssClassNames} from '../../const'
+class MarvelCharacters extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      characters: [],
+      offset: 0,
+      limit: 20,
+      total: 0,
+      isFetching: false
+    }
+    this.marvelService = getInstance()
+    this.renderCharacterTile = this.renderCharacterTile.bind(this)
+    this.fetchPageData = this.fetchPageData.bind(this)
+    this.handlePrevious = this.handlePrevious.bind(this)
+    this.handleNext = this.handleNext.bind(this)
+    this.handleChangePage = this.handleChangePage.bind(this)
+  }
+
+  componentDidMount() {
+    this.fetchPageData(this.state.offset, this.state.limit)
+  }
+
+  fetchPageData(offset, limit) {
+    this.setState({isFetching: true})
+    this.marvelService.getCharacters(offset, limit).then((response) => {
+      this.setState({
+        characters: response.results,
+        offset: response.offset,
+        limit: response.limit,
+        total: response.total
+      })
+    }).catch(error => {
+      console.error('MarvelCharacters error: ' + error)
+    }).finally(() => {
+      this.setState({isFetching: false})
+    })
+  }
+
+  renderCharacterTile(character) {
+    return <div key={`character${character.name}`} className={CssClassNames.tileContainer} style={{width: '300px'}}>
+      <img src={getImageUrl(character.thumbnail, MarvelImageFormats.landscape_large)} className={CssClassNames.tileImage} />
+      <div className={CssClassNames.tileBody}>
+        <h5 className={CssClassNames.tileTitle}>{this.props.showDetailLink ?
+          <Link to={`/characters/${character.id}`}>{character.name}</Link> : character.name}</h5>
+      </div>
+    </div>
+  }
+
+  handlePrevious(event) {
+    event.preventDefault()
+    this.fetchPageData(this.state.offset - this.state.limit, this.state.limit)
+  }
+
+  handleNext(event) {
+    event.preventDefault()
+    this.fetchPageData(this.state.offset + this.state.limit, this.state.limit)
+  }
+
+  handleChangePage(pageNumber) {
+    this.fetchPageData((pageNumber - 1) * this.state.limit, this.state.limit)
+  }
+
+  renderPagination() {
+    return <Pagination
+      offset={this.state.offset}
+      limit={this.state.limit}
+      total={this.state.total}
+      handlePrevious={this.handlePrevious}
+      handleNext={this.handleNext}
+      handleChangePage={this.handleChangePage}
+      isFetching={this.state.isFetching}
+    />
+  }
+
+  render() {
+    return <div>
+      <h1>Liste des personnages de Marvel</h1>
+      {this.renderPagination()}
+      <div className={CssClassNames.listContainer}>
+        {this.state.characters.map(character => {
+          return this.renderCharacterTile(character)
+        })}
+      </div>
+      {this.renderPagination()}
+    </div>
+  }
+}
+
+export default MarvelCharacters
---
+++ react-examples/test/src/jsx-demo.jsx
@@ -0,0 +1,6 @@
+const ReactComponent1 = <div>Test</div>
+
+const ReactComponent2 = <div>
+  <h1>Title</h1>
+  <ReactComponent1 />
+</div>
