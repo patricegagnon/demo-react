874bc654d379f4e89457f234507d1d82d1f972fc
Patrice Gagnon <patrice.gagnon@alithya.com>
2020-04-21 16:33:26 -0400

    Ajout de React exemple seul

---
+++ react-examples/webpack.config.js
@@ -0,0 +1,64 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const webpack = require('webpack')
+
+const path = require('path');
+module.exports = function(env) {
+  const appName = env.app
+  const server = env.server || 'local'
+  console.log('building ' + appName)
+  const configFilename = path.join(__dirname, `./build.${server}.js`)
+  const appConfig = require(configFilename)
+
+  const entryPath = path.join(__dirname,'src',appName + '.js')
+  return {
+    entry: {
+        main: ['@babel/polyfill', entryPath]
+    },
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: appName + '.js',
+        publicPath: '/'
+      },
+    devServer: {
+      publicPath: '/',
+      contentBase: path.join(__dirname, 'dist'),
+      inline: true,
+      port: 8085,
+      historyApiFallback:true,
+      clientLogLevel: 'debug',
+      before: function(app, server, compiler) {
+        app.get('*', function(req, res, next) {
+          console.log("serving " + req.url)
+          next()
+        });
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new CopyWebpackPlugin([ { from: 'src/public', to: './public'}]),
+      new CopyWebpackPlugin([ { from: 'src/caching', to: './caching'}]),
+      new webpack.DefinePlugin({
+        'envConfig.marvelProxyServiceBaseUrl': '"' + appConfig.marvelProxyServiceBaseUrl + '"'
+      }),
+    ],
+    devtool: "source-map"
+  };
+}
