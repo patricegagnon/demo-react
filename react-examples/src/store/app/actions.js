8ffdb830f3b96a99558b1ad3cd0a170c5a9d03c5
Patrice Gagnon <patrice.gagnon@alithya.com>
2020-04-22 12:56:41 -0400

    enlever le mode déconnecté des ReactExamples

--- react-examples/src/store/app/actions.js
+++ react-examples/src/store/app/actions.js
@@ -1,12 +1,9 @@
-import MarvelService, {getInstance} from "../../services/MarvelProxyService";
 
 
 export const Actions = {
-  SET_MARVEL_SERVICE_STATUS: 'SET_MARVEL_SERVICE_STATUS'
 }
 
 
 export const ActionCreators = {
-  setMarvelServiceStatus: (status) => ({type:Actions.SET_MARVEL_SERVICE_STATUS, status}),
 
 }
